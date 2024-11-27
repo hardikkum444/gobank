22465569c402cf696d6175347dccd8e25beb7b89
hardikkum444 <hardikkumawat444@gmail.com>
2024-11-27 20:24:14 +0530

    boiler plate code

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hardikkum444/gobank
 
 go 1.23.3
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,20 +1,63 @@
 package main
 
 import(
-    "fmt"
     "net/http"
+    "github.com/gorilla/mux"
+    "encoding/json"
 )
 
-type APIserver struct {
+type APIServer struct {
     listenAddr string
 }
 
-func NewAPIServer(listenAddr string) *APIserver {
-    return &APIserver {
+func writeJSON(w http.ResponseWriter, status int, v any) error {
+    w.WriteHeader(status)
+    w.Header().Set("Content-Type", "application/json")
+    return json.NewEncoder(w).Encode(v)
+}
+
+type ApiError struct {
+    Error string
+}
+
+type apifunc func(http.ResponseWriter, *http.Request) error
+
+func makeHTTPHandlerFunc(f apifunc) (http.HandlerFunc) {
+    return func(w http.ResponseWriter, r *http.Request) {
+        if err := f(w, r); err != nil {
+            writeJSON(w, http.StatusInternalServerError, ApiError{Error: err.Error()})
+        }
+    }
+}
+
+func newAPIServer(listenAddr string) *APIServer {
+    return &APIServer{
         listenAddr: listenAddr,
     }
 }
 
-func (s *APIserver) handleAccount(w http.ResponseWriter, r *http.Request) error {
+func (s *APIServer) Run() {
+    router := mux.NewRouter()
+
+    router.HandleFunc("/account", makeHTTPHandlerFunc(s.handleAccount))
+}
+
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+    return nil
+}
+
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+    return nil
+}
+
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+    return nil
+}
+
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+    return nil
+}
+
+func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
     return nil
 }
